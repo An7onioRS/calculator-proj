ed41d8548737ec356414ee09d5fc6f2cbc08c325
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-03-11 09:18:18 +0200

    Add references to buttons, operators and display

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-    let result;
+    let result = 0;
     switch (operator) {
         case '+':
             result = sum(num1, num2);
@@ -31,4 +31,20 @@ function operate(operator, num1, num2) {
             break;
         default:
             result = 'Invalid operator';
-}};
+    };
+    return result;
+}
+
+function addEventListenerList(list, event, fn) {
+    list.forEach(button => button.addEventListener(event, fn));
+};
+
+const displayValue = document.querySelector('.display-results');
+const operators = document.querySelectorAll('.operator');
+const buttons = document.querySelectorAll('.number'); 
+
+addEventListenerList(buttons, "click", function(e) {
+    displayValue.textContent += e.target.textContent;
+
+});
+
