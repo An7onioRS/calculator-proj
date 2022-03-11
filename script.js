0e6f2a4c9d1cf69e8155ccaafdeea41b4a092461
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-03-11 10:08:27 +0200

    Add logic to addEventListenerList to make the calculator work depending on what key is pressed

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-    let result = 0;
+    let result;
     switch (operator) {
         case '+':
             result = sum(num1, num2);
@@ -40,11 +40,41 @@ function addEventListenerList(list, event, fn) {
 };
 
 const displayValue = document.querySelector('.display-results');
-const operators = document.querySelectorAll('.operator');
-const buttons = document.querySelectorAll('.number'); 
+const buttons = document.querySelectorAll('button'); 
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+let equal = false;
 
 addEventListenerList(buttons, "click", function(e) {
-    displayValue.textContent += e.target.textContent;
+    const input = e.target.textContent;
 
+    if (equal) displayValue.textContent = "";
+
+    switch (e.target.classList[0]) {
+        case "number":
+            if (!operator) {
+                displayValue.textContent += input;
+                num1 += input;
+            }
+            else {
+                displayValue.textContent += input;
+                num2 += input;
+            }
+            break;
+        case "operator":
+            displayValue.textContent += input;
+            operator = input;
+            break;
+        case "clear":
+            displayValue.textContent = "";
+            break;
+        case "equal":
+            num1 = parseInt(num1);
+            num2 = parseInt(num2);
+            displayValue.textContent = operate(operator, num1, num2);
+            equal = true;
+            break;
+    }
 });
 
